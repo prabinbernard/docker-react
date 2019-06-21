1e2556bfdc24d50b83bdd646adcfb5f26dd06d70
prabin bernard <admin@infinity>
2019-06-21 15:59:01 +0100

    changed the app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Program abort!</h2>
+        <h2>Program Success!</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
